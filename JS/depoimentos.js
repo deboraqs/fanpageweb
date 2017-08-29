b1e16f85ec1875fdf18ed68dbb0823d1435290da
Nathan Andrade <nathanandrade20@gmail.com>
2017-08-29 18:44:03 -0300

    Mudanças no nav e Add materialboxed nas img

---
+++ JS/depoimentos.js
@@ -0,0 +1,20 @@
+var app = angular.module('TVGlobinho', ['ngCookies']);
+
+app.controller("DepoimentosController", function($scope, $cookies) {
+    if(!$.isEmptyObject($cookies.getObject("depoimentos"))){
+        $scope.depoimentos = $cookies.getObject("depoimentos");
+    }
+    else{
+        $scope.depoimentos = [{nome: "Gustavo Vilar", depoimento: "Choro todo dia sentindo falta."}];
+
+        $cookies.putObject("depoimentos", $scope.depoimentos);
+    }
+
+    $scope.adicionarDepoimento = function() {
+        $scope.depoimentos.push({nome: $scope.item.nome, depoimento: $scope.item.depoimento});
+
+        $cookies.putObject("depoimentos", $scope.depoimentos);
+
+        $scope.item.nome = $scope.item.depoimento = "";
+    };
+});
