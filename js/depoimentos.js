4ee0723b4c68deb74d9b08903179943ad0cca0cc
Gustavo Vilar de Farias <gvilardefarias@ymail.com>
2017-08-28 22:45:39 -0300

    Adição do armazenamento dos depoimentos nos cookies

--- js/depoimentos.js
+++ js/depoimentos.js
@@ -1,13 +1,22 @@
-var app = angular.module('TVGlobinho', []);
+var app = angular.module('TVGlobinho', ['ngCookies']);
 
-app.controller("DepoimentosController", function($scope) {
-    $scope.depoimentos = [
-        {nome: "Gustavo Vilar", depoimento: "Choro todo dia sentindo falta."}
-    ];
+app.controller("DepoimentosController", function($scope, $cookies) {
+	if(!$.isEmptyObject($cookies.getObject("depoimentos"))){
+    	$scope.depoimentos = $cookies.getObject("depoimentos");
+	}
+    else{
+    	$scope.depoimentos = [
+	        {nome: "Gustavo Vilar", depoimento: "Choro todo dia sentindo falta."}
+	    ];
+
+	    $cookies.putObject("depoimentos", $scope.depoimentos);
+    }
 
     $scope.adicionarDepoimento = function() {
-        $scope.depoimentos.push({nome: $scope.item.nome,
+    	$scope.depoimentos.push({nome: $scope.item.nome,
         						 depoimento: $scope.item.depoimento});
+
+    	$cookies.putObject("depoimentos", $scope.depoimentos);
 
         $scope.item.nome = $scope.item.depoimento = "";
     };
