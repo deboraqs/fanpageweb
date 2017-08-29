11f5dc3acff66884ab5e3ae9aa3d33a53aa9bbf3
Gustavo Vilar de Farias <gvilardefarias@ymail.com>
2017-08-28 23:04:34 -0300

    Corrigindo identação

--- js/depoimentos.js
+++ js/depoimentos.js
@@ -1,22 +1,19 @@
 var app = angular.module('TVGlobinho', ['ngCookies']);
 
 app.controller("DepoimentosController", function($scope, $cookies) {
-	if(!$.isEmptyObject($cookies.getObject("depoimentos"))){
-    	$scope.depoimentos = $cookies.getObject("depoimentos");
-	}
+    if(!$.isEmptyObject($cookies.getObject("depoimentos"))){
+        $scope.depoimentos = $cookies.getObject("depoimentos");
+    }
     else{
-    	$scope.depoimentos = [
-	        {nome: "Gustavo Vilar", depoimento: "Choro todo dia sentindo falta."}
-	    ];
+        $scope.depoimentos = [{nome: "Gustavo Vilar", depoimento: "Choro todo dia sentindo falta."}];
 
-	    $cookies.putObject("depoimentos", $scope.depoimentos);
+        $cookies.putObject("depoimentos", $scope.depoimentos);
     }
 
     $scope.adicionarDepoimento = function() {
-    	$scope.depoimentos.push({nome: $scope.item.nome,
-        						 depoimento: $scope.item.depoimento});
+        $scope.depoimentos.push({nome: $scope.item.nome, depoimento: $scope.item.depoimento});
 
-    	$cookies.putObject("depoimentos", $scope.depoimentos);
+        $cookies.putObject("depoimentos", $scope.depoimentos);
 
         $scope.item.nome = $scope.item.depoimento = "";
     };
