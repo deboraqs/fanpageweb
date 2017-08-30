4a51f73a196af69b322be0cd0929b08370ba091c
LucasAlvesMartins <30660845+LucasAlvesMartins@users.noreply.github.com>
2017-08-29 21:31:10 -0300

    Add files via upload

--- depoimentos.js
+++ depoimentos.js
@@ -6,8 +6,9 @@ app.controller("DepoimentosController", function($scope, $cookies) {
     }
     else{
         $scope.depoimentos = [{nome: "Gustavo Vilar", depoimento: "Choro todo dia sentindo falta."},
-                              {nome: "Nathan Andrade", depoimento: "Venho por meio desta, deixar meu descontentamento com a Rede Esgoto de Televisão. A TV Globinho era um dos melhores programas que já exibido na rede aberta nacional, e foi substituído por um programa bem micão. Não boto fé na Fátima, mas nós da quentes sabemos que quem tirou a panca não foi ela e sim a emissora. Fora Rede Globo!! É fixa, parceiro."}];
-
+                              {nome: "Nathan Andrade", depoimento: "Venho por meio desta, deixar meu descontentamento com a Rede Esgoto de Televisão. A TV Globinho era um dos melhores programas que já exibido na rede aberta nacional, e foi substituído por um programa bem micão. Não boto fé na Fátima, mas nós da quentes sabemos que quem tirou a panca não foi ela e sim a emissora. Fora Rede Globo!! É fixa, parceiro."}
+                              {nome: "Raphael Victor", depoimento: "Se eu quisesse me encontrar com a Fátima, eu iria a procura dela. Mas não, eu não quero. #PrayForTVGlobinho"}];
+                              
         $cookies.putObject("depoimentos", $scope.depoimentos);
     }
 
